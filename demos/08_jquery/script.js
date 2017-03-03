1870d2377fb21dc66fb2067fcfb3d36305ba5824
marieotsuka <marotska@gmail.com>
2017-03-03 12:08:15 -0500

    js code

--- demos/08_jquery/script.js
+++ demos/08_jquery/script.js
@@ -3,17 +3,17 @@ console.log("hello from external script.js");
 $(document).ready(function(){
 
 	$('.button').click(function(){
+		console.log("button clicked");
 		$('.hi').fadeToggle(2000);
 	});
 
 	$('#play').click(function(){
 		$('.circle').addClass('play');
-	})
+	});
 
 	$('#stop').click(function(){
 		$('.circle').removeClass('play');
-	})
-
+	});
 
 });
 
