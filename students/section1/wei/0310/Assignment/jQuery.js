153e2385fde820bdfc1c7ac24dcc74cb1fbe7b12
王 偉澔 <drxk41012@gmail.com>
2017-03-10 03:56:01 -0500

    3/10-2

--- students/section1/wei/0310/Assignment/jQuery.js
+++ students/section1/wei/0310/Assignment/jQuery.js
@@ -1,26 +1,9 @@
 $(document).ready(function(){
-	var counter = 0;
-
-	var five = "5";
-	var five2 = 5;
-
-	$('#cat').click(function(){
-		counter++; 
-		console.log(counter);
-		if (counter===1){
-			$(this).animate({top:'+=500px'})
-		}
-		if (counter===2){
-			$(this).animate({left:'+=600px'})
-		}
+	$("#textSwitch").click(function(){
+		$("#text").toggle();
 	});
-		
-	// $('.stepOne').click(function(){
-	// 	console.log("stpone");
-	// 	.removeClass('stepOne').addClass('stepTwo');
-	// });
-
-	
-
+	$("#contactBtn h1").click(function(){
+		this.empty();
+	});
 });
 
