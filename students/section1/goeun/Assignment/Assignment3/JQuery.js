981ad421b37f45e1b547a6ecc0acd933718a15c8
goeun0179 <goeun0179@gmail.com>
2017-03-10 08:16:53 -0500

    Study

---
+++ students/section1/goeun/Assignment/Assignment3/JQuery.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	
+	$('.circle').click(function(){
+		$('.circle2').animate({left: '2000px'});
+		
+	});
+		
+	// $('.stepOne').click(function(){
+	// 	console.log("stpone");
+	// 	.removeClass('stepOne').addClass('stepTwo');
+	// });
+
+	
+
+});
