ec7bd729bf263aa69bf1a36f4887193cd933d541
jhuashao <joshua.h.shao@gmail.com>
2017-03-17 12:41:37 -0400

    new type/ new layout

---
+++ students/section2/joshuashao/demos/08_jquery/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+  var banana = $("h3").html();
+
+  console.log(banana);
+
+
+  $("#trigger").click(function(){
+      
+      $("h3").html("This <em>is</em> replaced text");
+      $("h2").append(" this is appended via jQuery");  
+
+
+      var h3TextColor = $("h3").css("color");
+      console.log("The heading 3 text color is " + h3TextColor);
+
+      $("h3").css("color", "red");
+      
+      h3TextColor = $("h3").css("color");
+      console.log('After changing, "change" the heading 3 text color is' + h3TextColor);
+  });
+
+  $(".dropdown").click(function(){
+  	var text = $("dd",this);
+  	var icon = $(this).children('.toggle');
+    
+    text.slideToggle(400, function (){
+    	if (text.is(':visible')) {
+    	             icon.text('–');                
+    	        } else {
+    	             icon.text('+');                
+    	        }        
+      console.log("dropdown opened");
+    });
+
+    $(this).toggleClass("selected");
+  });
+});
+console.log(8+8);
