2df112d244c2aeeb367bd17be0e5d8957d2e3ac3
marieotsuka <marotska@gmail.com>
2017-04-07 10:36:09 -0400

    responsive layouts and animations

--- demos/08_jquery/script.js
+++ demos/08_jquery/script.js
@@ -3,21 +3,19 @@ console.log("hello from external script.js");
 $(document).ready(function(){
 
 	$('.button').click(function(){
-		console.log("button clicked");
-		$('.hi').fadeToggle(2000);
+		$('.hi').fadeToggle(1200);
 	});
 
 	$('#start').click(function(){
-		$('.circle').addClass('play');
+		$('#target').addClass('play');
 	});
-
+	
 	$('#stop').click(function(){
-		$('.circle').removeClass('play');
+		$('#target').removeClass('play');
 	});
 
-	$('.element').hover(function(){
-		// console.log("element hover");
-		$(this).toggleClass('red');
+	$('#red').click(function(){
+		$(this).toggleClass('play');
 	});
 
 });
