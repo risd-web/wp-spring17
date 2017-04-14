0d2d93c60967d71c4e2b794fe9b5c14cc3021a62
sophiaspitulnik <sspituln@risd.edu>
2017-04-14 02:39:30 -0400

    4/14/2017 2;39

---
+++ students/section2/sophiaspitulnik/Finalish/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $(".dropdown").click(function(){
+    var text = $("dd",this);
+    var icon = $(this).children('.toggle');
+    
+    text.slideToggle(400, function (){
+      if (text.is(':visible')) {
+                   icon.text('–');                
+              } else {
+                   icon.text('+');                
+              }        
+      console.log("dropdown opened");
+    });
+
+    $(this).toggleClass("selected");
+  });
+});
+console.log(8+8);
