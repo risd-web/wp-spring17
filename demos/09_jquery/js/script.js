8396bfdb50a918d9f3596c68672c25adb4a2849f
marieotsuka <marotska@gmail.com>
2017-05-05 15:37:30 -0400

    jquery

--- demos/09_jquery/js/script.js
+++ demos/09_jquery/js/script.js
@@ -1,4 +1,4 @@
-console.log("hello from external script.js");
+console.log("hello from external script.js"); //just to check that your script is loading in console
 
 $(document).ready(function(){
 
