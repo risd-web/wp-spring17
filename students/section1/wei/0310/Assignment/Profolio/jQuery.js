9f0f8241b60a23dda3706309f1566b6492df2d67
王 偉澔 <drxk41012@gmail.com>
2017-06-21 11:23:30 -0400

    621

--- students/section1/wei/0310/Assignment/Profolio/jQuery.js
+++ students/section1/wei/0310/Assignment/Profolio/jQuery.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$("#textSwitch").click(function(){
 		$("#text").toggle(500);
 	});
+	
 	$("#contactBtn").mouseover(function(){
 		 $(this).empty().append("<h1>wwang03@risd.edu</h1>");
 	});
